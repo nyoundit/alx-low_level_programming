6eacf062ce03631d07b20333602807b0d525b811
nyoundit <nyoundit@gmail.com>
2022-03-17 21:16:19 +0300

    Betty Corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
-int main(void) {
+/*
+* Function destricpioon
+* another descripton
+*/
+int main(void)
+{
 
 	printf("\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return 0;
+	return (0);
 }
