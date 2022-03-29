40a574cc75c28229bcc9d5cf3f02ee3fabd49227
nyoundit <nyoundit@gmail.com>
2022-03-29 18:36:00 +0300

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - check the code for Holberton School students.
+ * @n: is a pointer int type
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}#include "holberton.h"
+
+/**
+ * reset_to_98 - check the code for Holberton School students.
+ * @n: is a pointer int type
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
