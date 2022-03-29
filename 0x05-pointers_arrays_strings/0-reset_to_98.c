af3bde1d73faf2ab0607c72b31d817d62e03b574
nyoundit <nyoundit@gmail.com>
2022-03-29 18:40:23 +0300

    0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -9,15 +9,4 @@
 void reset_to_98(int *n)
 {
 *n = 98;
-}#include "holberton.h"
-
-/**
- * reset_to_98 - check the code for Holberton School students.
- * @n: is a pointer int type
- * Return: Always 0.
- */
-
-void reset_to_98(int *n)
-{
-*n = 98;
 }
