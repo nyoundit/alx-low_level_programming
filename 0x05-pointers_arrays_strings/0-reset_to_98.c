610f6cd2d850446288f0848d6c7a8db2797798e7
nyoundit <nyoundit@gmail.com>
2022-03-29 20:34:59 +0300

    0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,7 @@
 #include "main.h"
-
 /**
- * reset_to_98 - check the code for Holberton School students.
- * @n: is a pointer int type
- * Return: Always 0.
+ * reset_to_98 - main
+ * @n: - pinter to int
  */
 
 void reset_to_98(int *n)
